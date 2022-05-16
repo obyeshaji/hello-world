911ed4d8cd0e4df69dd34a96121c3332a495320d
obyeshaji <98787412+obyeshaji@users.noreply.github.com>
2022-05-16 19:49:36 -0400

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,39 @@
+package lecture3;
+
+public class Lab1 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x,y; // initializing x and y 
+		x=5;
+		y=6;
+		System.out.println("The value of x is "+x);
+		System.out.println("the value of y is " +y );
+		System.out.println("---------------");
+		x=x+1;
+		System.out.println("the value of x is "+x);
+		System.out.println("the value of y is "+y);
+		
+		String sentence = "My first line in java";
+		
+		String Name = "Obye Shaji ";
+		String Email ="oshaji@nyit.edu ";
+		String Id = "12345 ";
+		
+		
+		System.out.println(Name+Email+Id);
+		
+
+	
+	
+	
+	
+	
+	}
+
+	
+	
+	
+}
